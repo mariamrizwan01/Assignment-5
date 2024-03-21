1b3236f34fd179f977aa5f2945a20ee7d0c6c453
mariamrizwan01 <mariam.rizwan01@gmail.com>
2024-03-21 13:51:38 +0500

    Assignment 5

--- Question4.dart
+++ Question4.dart
@@ -1,8 +1,8 @@
 void main() {
-  List num = [8, 3, 4, 6, 80, 90];
-  if (num.isEmpty) {
+  List number = [8, 3, 4, 6, 80, 90];
+  if (number.isEmpty) {
     print("The List is Empty");
-  } else {
+  } else if (number.isNotEmpty) {
     print("The List is not Empty");
   }
 }
