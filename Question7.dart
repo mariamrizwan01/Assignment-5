55209368ad168a03d3f441f901f89d461d6fa3f0
mariamrizwan01 <mariam.rizwan01@gmail.com>
2024-03-23 21:31:37 +0500

    Assignment 5

---
+++ Question7.dart
@@ -0,0 +1,15 @@
+void main() {
+  Map<String, int > CityPopulation = {
+    "Karachi": 95878899,
+    "Islamabad": 567582494,
+    "Lahore": 45039485,
+    "Multan": 890881,
+    "Sailkot": 655655,
+    "Okara": 357953,
+  };
+
+  print('${CityPopulation[1]["City"]}, ${CityPopulation[3]["Population"]}');
+  print(CityPopulation[0]["City"]);
+ 
+}
+
